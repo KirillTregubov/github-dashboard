241622636c9b4130a423b69d8eea1cfafbdcb5d4
Kirill Tregubov <31662934+KirillTregubov@users.noreply.github.com>
2023-03-07 06:28:57 -0500

    Update token.ts

--- pages/api/token.ts
+++ pages/api/token.ts
@@ -1,15 +1,11 @@
 import { getToken } from 'next-auth/jwt'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-const secret = process.env.NEXTAUTH_SECRET
-
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  // if using `NEXTAUTH_SECRET` env variable, we detect it, and you won't actually need to `secret`
-  // const token = await getToken({ req })
-  const token = await getToken({ req, secret })
+  const token = await getToken({ req, raw: true })
   console.log('JSON Web Token', token)
   res.end()
 }
