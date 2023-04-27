6424e3a7c11fddbd3a414c219162dc5bd08353d2
Kirill Tregubov <31662934+KirillTregubov@users.noreply.github.com>
2023-04-27 16:34:21 -0400

    trying to isolate error

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,25 @@
 import Auth from '@/components/auth'
-import RepoList from '@/components/repolist'
-import { getSession } from '@/lib/auth'
+// import RepoList from '@/components/repolist'
+// import { getSession } from '@/lib/auth'
 
 export default async function Home() {
-  const session = await getSession()
+  //   const session = await getSession()
 
   return (
     <main>
       {/* @ts-expect-error Async Server Component */}
       <Auth />
-      {session ? (
-        <div>
-          {/* @ts-expect-error Async Server Component */}
-          <RepoList />
-        </div>
-      ) : (
-        <div className="m-2">
-          <p>Welcome to dashboard! Please authenticate to continue.</p>
-        </div>
-      )}
     </main>
   )
 }
+
+// {session ? (
+//     <div>
+//       {/* @ts-expect-error Async Server Component */}
+//       <RepoList />
+//     </div>
+//   ) : (
+//     <div className="m-2">
+//       <p>Welcome to dashboard! Please authenticate to continue.</p>
+//     </div>
+//   )}
