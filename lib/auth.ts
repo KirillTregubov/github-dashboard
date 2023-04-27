6660481bbaf97b19c1827e634d537ebb3cb0032a
Kirill Tregubov <31662934+KirillTregubov@users.noreply.github.com>
2023-04-27 17:03:18 -0400

    Update auth.ts

--- lib/auth.ts
+++ lib/auth.ts
@@ -3,6 +3,7 @@ import type { NextAuthOptions } from 'next-auth'
 import GithubProvider, { GithubProfile } from 'next-auth/providers/github'
 
 export const authOptions: NextAuthOptions = {
+  secret: process.env.NEXTAUTH_SECRET,
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_CLIENT_ID,
